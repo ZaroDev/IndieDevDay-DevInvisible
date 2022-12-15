d265144682db1bfbe8beb5fb7ea8414d8d53c690
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2022-12-15 23:52:01 +0100

    money

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public static class MoneyResource
 {
     private static int moneyAmount;
@@ -16,4 +17,41 @@ public static class MoneyResource
         return moneyAmount;
     }
 
+}
+
+public class Resources : MonoBehaviour
+{
+    public Image moneyImg;
+    public Sprite[] moneySprites;
+
+    public TextMeshProUGUI moneyDisplay;
+    
+    void start()
+    {
+        if(moneySprites != null)
+        {
+            moneyImg.sprite = moneySprites[0];
+        }
+        
+    }
+    
+    void FixedUpdate()
+    {
+        MoneyResource.AddMoneyAmount(1);
+        DisplayMoney();
+
+        if(MoneyResource.GetMoneyAmount()>500)
+        {
+            moneyImg.sprite = moneySprites[1];
+        }
+        if(MoneyResource.GetMoneyAmount()>1000)
+        {
+            moneyImg.sprite = moneySprites[2];
+        }
+    }
+
+    void DisplayMoney()
+    {
+        moneyDisplay.text = "$ "+MoneyResource.GetMoneyAmount();
+    }
 }
