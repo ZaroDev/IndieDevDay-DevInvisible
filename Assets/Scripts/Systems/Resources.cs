604864eaa0ccc9ea72ffa66b38fea9c2f12eaf0c
Victor <victorfalcon796@gmail.com>
2022-12-16 10:33:22 +0100

    Tilemaps

---
+++ Assets/Scripts/Systems/Resources.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public static class MoneyResource
+{
+    private static int moneyAmount;
+
+    public static void AddMoneyAmount(int amount)
+    {
+        moneyAmount += amount;
+    }
+    public static void SubstractMoneyAmount(int amount)
+    {
+        moneyAmount -= amount;
+    }
+
+    public static int GetMoneyAmount()
+    {
+        return moneyAmount;
+    }
+
+}
+
+public class Resources : MonoBehaviour
+{
+    public Image moneyImg;
+    public Sprite[] moneySprites;
+
+    public TextMeshProUGUI moneyDisplay;
+
+    void Start()
+    {
+        if (moneySprites != null)
+        {
+            moneyImg.sprite = moneySprites[0];
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        MoneyResource.AddMoneyAmount(1);
+        DisplayMoney();
+
+        if (MoneyResource.GetMoneyAmount() > 500)
+        {
+            moneyImg.sprite = moneySprites[1];
+        }
+        if (MoneyResource.GetMoneyAmount() > 1000)
+        {
+            moneyImg.sprite = moneySprites[2];
+        }
+        if (MoneyResource.GetMoneyAmount() > 1100)
+        {
+            MoneyResource.SubstractMoneyAmount(600);
+        }
+    }
+
+    void DisplayMoney()
+    {
+        moneyDisplay.text = "$ " + MoneyResource.GetMoneyAmount();
+    }
+}
