67becd010470fcc13be6131b9ced9c69568e3389
Victor <victorfalcon796@gmail.com>
2022-12-21 10:05:39 +0100

    Basement

---
+++ Assets/Scripts/Scenes/FloorManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    public GameObject floor;
+    public GameObject basement;
+    bool IsBasement = false;
+    void Start()
+    {
+        basement.gameObject.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SwitchFloor();
+        }
+    }
+
+    void SwitchFloor()
+    {
+        IsBasement = !IsBasement;
+
+        if (IsBasement)
+        {
+            basement.SetActive(false);
+            floor.SetActive(true);
+        }
+        else
+        {
+            basement.SetActive(true);
+            floor.SetActive(false);
+        }
+    }
+
+}
