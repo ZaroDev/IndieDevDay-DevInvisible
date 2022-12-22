2d1a1584f4d9af337b2f5df499712a18d8e7f7a6
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2022-12-22 21:42:07 +0100

    notification button

---
+++ Assets/Scripts/UI/MessagesUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagesUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MessageMenu;
+
+    private bool toggleMessageMenu;
+
+    void Start()
+    {
+        toggleMessageMenu = false;
+    }
+    public void ToggleMenu()
+    {
+        if(toggleMessageMenu)
+        {
+            toggleMessageMenu = false;
+            MessageMenu.SetActive(false);
+
+        }   
+        else if(!toggleMessageMenu)
+        {
+            toggleMessageMenu = true;
+            MessageMenu.SetActive(true);
+        }
+    }
+
+    
+}
