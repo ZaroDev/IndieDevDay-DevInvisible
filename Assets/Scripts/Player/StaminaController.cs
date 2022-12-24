33d8e03721c5b05e443fc8ef3e578a9c72866477
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2022-12-24 21:08:21 +0100

    w

--- Assets/Scripts/Player/StaminaController.cs
+++ Assets/Scripts/Player/StaminaController.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 public class StaminaController : GameSystem
 {
     [SerializeField]
-    public static float MaxStamina = 1000f;
-    public static float Stamina = 1000f;
+    public static float MaxStamina = 700f;
+    public static float Stamina = 700f;
 
     public static Action<float> OnStaminaUse;
     public static Action<float> OnStaminaRestore;
     public static void Use(float amount)
     {
-        if(Stamina>0) Stamina -= amount;
+        if (Stamina > 0) Stamina -= amount;
         Mathf.Clamp(Stamina, 0, MaxStamina);
         OnStaminaUse?.Invoke(Stamina);
-    
+
     }
     public static void RestoreStamina(float amount)
     {
-        if(Stamina<1000)    Stamina += amount;
+        if (Stamina < 700) Stamina += amount;
         OnStaminaRestore?.Invoke(Stamina);
     }
 
